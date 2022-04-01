3233882c8fe9c14583e31ac89a60e2878af8aecc
yaredtsy <34584266+yaredtsy@users.noreply.github.com>
2022-04-01 12:10:00 -0700

    Added custome user model

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name','last_name','last_login','date_joined','is_active')
+    list_display_links = ('email', 'username', 'first_name')
+    readonly_fields = ('last_login','date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    filedsets = ()
+admin.site.register(Account,AccountAdmin)
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from decart.views import home
+from django.conf.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
 ]
+
+urlpatterns += static.static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
