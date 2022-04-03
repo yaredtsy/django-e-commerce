b631075df67c831546d18fed09ad58ec856ee83b
yaredtsy <34584266+yaredtsy@users.noreply.github.com>
2022-04-03 04:52:32 -0700

    Product variation added

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from store.models import Product
+from store.models import Product,Variation
 from .models import Cart,CartItem
 from django.core.exceptions import ObjectDoesNotExist 
+
 # Create your views here.
 
 def _cart_id(request):
@@ -12,6 +13,19 @@ def _cart_id(request):
 
 def add_cart(request,product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(product=product,variation_category__iexact=key,variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+        
+    
     try:
         cart = Cart.objects.get(cart_id= _cart_id(request))
 
